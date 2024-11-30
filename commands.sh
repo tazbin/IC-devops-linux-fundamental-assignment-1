b343e8d23043d6c4423f7b324144eff6fe455f0b
tazbin <mdtazbinur@gmail.com>
2024-12-01 00:01:45 +0600

    Add package installation commands & screenshots

--- commands.sh
+++ commands.sh
@@ -150,3 +150,20 @@ cat hardlink.txt
 
 # Find all .txt files in your home directory
 find ~/ -type f -name "*.txt"
+
+###############################################
+# Package installation
+###############################################
+
+# Update repo cache using apt/apt-get
+apt update
+
+# Install a package named tree
+apt install tree
+
+# Install gcloud CLI tool using apt
+apt install apt-transport-https ca-certificates gnupg curl
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /usr/share/keyrings/cloud.google.gpg
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+apt update && apt install google-cloud-cli
+gcloud init
