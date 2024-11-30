639fd703e63d6e4c7049ed04ad38da3b27cc87ee
tazbin <mdtazbinur@gmail.com>
2024-11-30 17:55:05 +0600

    Add file system commands & screenshots

---
+++ commands.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+###############################################
+# File System Navigation
+###############################################
+
+# List the contents of the home directory.
+ls ~
+
+# Change the current directory to /var/log and list its contents.
+cd /var/log
+ls
+
+# Find and display the path to the bash executable.
+which bash
+
+# Find the current shell
+echo $SHELL
